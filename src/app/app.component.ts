7565e9bcd0a726380f9af92690c6958f03b8f6d2
Mayank <mayankgoyal.btprod20@pec.edu.in>
2024-08-24 23:03:10 +0530

    dashboard page completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatIcon } from '@angular/material/icon';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatIconModule, MatIcon],
+  imports: [CommonModule, RouterOutlet, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
