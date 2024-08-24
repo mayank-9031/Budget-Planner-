ea25b83c932e172f3923305cb1167586e64448f8
Mayank <mayankgoyal.btprod20@pec.edu.in>
2024-08-24 17:18:42 +0530

    side navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatIconModule, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
